ad9620b851cb33ee139bb1214bada8eac1423749
camihm1106 <131406052+camihm1106@users.noreply.github.com>
2023-04-24 23:31:09 -0600

    Add files via upload
    Codigo final

--- codigo.py
+++ codigo.py
@@ -8,6 +8,13 @@ print("Iniciaremos creando la agenda previa")
 #esta funcion nos da todos los apartados en una lista
 
 def agenda():
+    """Esta funcion pregunta por los apartados y puntos a tratar de los apartados para almacenarlos en diferentes listas
+
+    Returns:
+        list: lista_agenda: retorna los apartados y puntos a tratar juntos
+              lista_apartados: retorna solo los apartados
+              lista_punto: retorna solo los puntos a tratar sin ningun apartado
+    """
     apartado=str(input("Agregue un apartado: "))
     lista_agenda=[]
     lista_apartados=[]
@@ -68,7 +75,7 @@ def agenda():
     return (lista_agenda, lista_apartados, lista_punto)
 
 lista_agenda2, lista_apartados2, lista_puntos2=agenda()
-print ("Agenda: ")
+print ("\n Agenda: ")
 print (lista_agenda2)
 
 def Participantes():
@@ -91,16 +98,21 @@ lista_participantes=Participantes()
 print("Lista de los participantes:")
 print('\n'.join(map(str,lista_participantes)))
 
+print("Seguimos con el Registro de los participantes")
+
 def reconocedor_voz():
-     apartado=lista_apartados2
-     puntos=lista_puntos2
-     agend=lista_agenda2
-     participante=lista_participantes
-     reconocimiento_voz=[]
-     cont=0 #contador para los puntos
-     cont2=0 #contador para el reconocimiento de voz
-     preg_iniciadora=str(input("¿Desea empezar la reunion? Responder 'si' o 'no': "))
-     while preg_iniciadora=='si':
+    """Esta funcion se encarga de relacionar el texto del reconocedor de voz con el apartado, puntos a tratar, participante 
+    y la hora en que se habla
+
+    Returns:
+        list: retorna todo lo dicho con el apartado, puntos a tratar y participante a lo largo de la sesion
+    """
+    apartado=lista_apartados2
+    puntos=lista_puntos2
+    participante=lista_participantes
+    reconocimiento_voz=[]
+    preg_iniciadora=str(input("¿Desea empezar la reunion? Responder 'si' o 'no': "))
+    while preg_iniciadora=='si':
         print ("Lista de apartados: ")
         for i, item in enumerate(apartado, start=0): #Añadimos numeros a cada apartado para ser escogido por el num
             print(f"{i}. {item}")
@@ -203,7 +215,98 @@ def reconocedor_voz():
                         print("No se pudo obtener respuesta desde el servicio de Google Speech Recognition: {0}".format(e))
             reconocimiento_voz[-1].append(l)
             preg_iniciadora=str(input("¿Desea seguir la reunion? Responder 'si' o 'no': "))
-        reconocimiento_voz=('\n'.join(map(str,reconocimiento_voz)))
         return (reconocimiento_voz)
 l_reportes=reconocedor_voz()
-print (l_reportes)
+
+def imprimir_conversaciones(conversaciones):
+    """Se encarga de imprimir las conversaciones con su apartado, punto a tratar, participante, hora inicio y el
+      texto que el reconocimiento de voz imprimio
+
+    Args:
+        conversaciones (list): lista ordenada de apartado, punto a tratar, participante, hora inicio y el texto reconocido
+    """
+    for conversacion in conversaciones:
+        categoria, subcategoria, usuario, timestamp, mensajes = conversacion
+        print(f"{categoria}:")
+        print(f"{subcategoria}:")
+        print(f"{usuario}: {timestamp} {', '.join(mensajes)}")
+
+
+def contar_palabras_por_usuario(listaConInformación):
+    """Esta funcion imprime la cantidad de palabras total que dijo un participante a lo largo de la reunion
+
+    Args:
+        listaConInformaci (list): lista ordenada de apartado, punto a tratar, participante, hora inicio y el texto reconocido
+         del reconocimiento de voz
+    """
+    print("Se imprime de orden desendente las personas que más palabras dijeron\n")
+    palabras_por_usuario = {}
+    for mensaje in listaConInformación:
+        usuario = mensaje[2]
+        palabras = mensaje[4]
+        if usuario not in palabras_por_usuario:
+            palabras_por_usuario[usuario] = 0
+        palabras_por_usuario[usuario] += sum([len(palabra.split()) for palabra in palabras])
+    palabras_por_usuario_ordenado = sorted(palabras_por_usuario.items(), key=lambda x: x[1], reverse=True)
+    for usuario, cantidad_palabras in palabras_por_usuario_ordenado:
+        print(f"{usuario}: {cantidad_palabras} palabras")
+
+def contar_intervenciones_por_punto_apartado(listaConInformación):
+    """Funcion que imprime la cantidad de veces que participo una persona en cada punto a tratar del apartado
+
+    Args:
+        listaConInformaci (list): lista ordenada de apartado, punto a tratar, participante, hora inicio y el texto reconocido
+         del reconocimiento de voz
+    """
+    intervenciones_por_punto_apartado = {}
+    for mensaje in listaConInformación:
+        punto = mensaje[0]
+        apartado = mensaje[1]
+        usuario = mensaje[2]
+        if punto not in intervenciones_por_punto_apartado:
+            intervenciones_por_punto_apartado[punto] = {}
+        if apartado not in intervenciones_por_punto_apartado[punto]:
+            intervenciones_por_punto_apartado[punto][apartado] = {}
+        if usuario not in intervenciones_por_punto_apartado[punto][apartado]:
+            intervenciones_por_punto_apartado[punto][apartado][usuario] = 0
+        intervenciones_por_punto_apartado[punto][apartado][usuario] += 1
+    for punto, apartados in intervenciones_por_punto_apartado.items():
+        print(punto + ":")
+        for apartado, usuarios in apartados.items():
+            print("\n"+ apartado + ":\n")
+            for usuario, cantidad_intervenciones in usuarios.items():
+                print(f"{usuario}: {cantidad_intervenciones} intervenciones")
+
+def reportes():
+    """Funcion que pregunta que reporte desea imprimir
+    """
+    print("1: Reporte 1 \n 2: Reporte 2 \n 3: Reporte 3")
+    
+    indice=int(input("\nIngrese el índice correspondiente al reporte que desea realizar: "))
+    if indice==1:
+        print("\n Se realizará el reporte 1\n")
+        imprimir_conversaciones(l_reportes)
+    elif indice==2:
+            print("\n Se realizará el reporte 2\n")
+            contar_palabras_por_usuario(l_reportes)
+    elif indice==3:
+            print("\n Se realizará el reporte 3\n")
+            contar_intervenciones_por_punto_apartado(l_reportes)
+
+
+def continuar_reportes():
+    """funcion que pregunta si desea imprimir algun otro reporte 
+    """
+    seguir_reportes=1
+    while seguir_reportes==1:
+        respuesta = input('¿Desea seguir imprimiendo reportes? (o no si no lo desea): ')
+        if respuesta == "si":
+            print("\n")
+            reportes()                       
+        else:
+            seguir_reportes+=1
+            break
+
+reportes()
+continuar_reportes()
+print("\n Ha finalizado el programa")
